2a230d7b64607058540cb5b4955b6b9fbb94eb9f
yannis <thimotheeleclerc@gmail.com>
2018-11-22 15:43:03 +0100

    début springcontroller

--- src/main/java/tp_mif03/Controller/SpringController.java
+++ src/main/java/tp_mif03/Controller/SpringController.java
@@ -1,4 +1,16 @@
 package tp_mif03.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+
+import javax.servlet.ServletContext;
+
+@Controller
 public class SpringController {
+    private ServletContext sContext;
+
+    public String Getmessage(String nomsalon,ModelMap m){
+
+    }
+
 }
